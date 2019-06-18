f26a85c1994525b0bbffd72b53f9ed07308a6009
michellec <michellecruz57@gmail.com>
2019-06-18 18:39:21 -0400

    Starting CSS Changes

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ initMap = () => {
         zoom: 12,
         scrollWheelZoom: false
       });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
     mapboxToken: 'pk.eyJ1IjoibWljaGVsbGVjcnV6NTciLCJhIjoiY2p4MmJuamM2MDFhYTQ0cmY5dWdmbXBnNCJ9.XIJV1yEOsyY2U_M59T_fbQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
